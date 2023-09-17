3735c437098762b91ea006222d0f36fbd3df7296
Charlotte Wang <yanjingliebe@icloud.com>
2023-09-16 21:39:08 -0400

    react top loading bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@
 //rcc tab
 //new terminal --> npm start
 //new terminal --> npm install react-router-dom@5.2.0
+// npm i react-top-loading-bar
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
@@ -18,45 +19,50 @@ import {
   Route,
   Link
 } from "react-router-dom"; //npm install react-router-dom: https://v5.reactrouter.com/web/guides/quick-start
-
+import LoadingBar from 'react-top-loading-bar' //https://www.npmjs.com/package/react-top-loading-bar
 
 export default class App extends Component {
   // a = "Charlotte";
   // let {title,description} = this.props;
+  state = { progress: 0 }
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
   render() {
     return (
       // <div>{this.a}! This App is made using class based component</div> //class component so use this.a
       <>
         <Router>
           <Navbar />
+          <LoadingBar color='#f11946' progress={this.state.progress} />
           <Switch>
             <Route exact path="/"> {/*exact path is used to avoid the problem of home page being displayed on every page*/}
-              <News country="us" pageSize={12} category="general" key="general" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="general" key="general" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/business">
-              <News country="us" pageSize={12} category="business" key="business" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="business" key="business" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/technology">
-              <News country="us" pageSize={12} category="technology" key="technology" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="technology" key="technology" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/science">
-              <News country="us" pageSize={12} category="science" key="science" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="science" key="science" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/sports">
-              <News country="us" pageSize={12} category="sports" key="sports" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="sports" key="sports" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/health">
-              <News country="us" pageSize={12} category="health" key="health" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="health" key="health" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
             <Route exact path="/entertainment">
-              <News country="us" pageSize={12} category="entertainment" key="entertainment" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
+              <News setProgress={this.setProgress} country="us" pageSize={12} category="entertainment" key="entertainment" /> {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
             </Route>
 
           </Switch>
 
         </Router>
         {/* <Navbar /> */}
-        {/* <News country="us" pageSize={12} category="technology" />  */}
+        {/* <News setProgress={this.setProgress}  country="us" pageSize={12} category="technology" />  */}
         {/*passing props and change category here to change content science/business/technology/entertainment/general/health/sports*/}
       </>
     )
--- src/Components/News.js
+++ src/Components/News.js
@@ -308,12 +308,16 @@ export class news extends Component {
         console.log("Hello I am a constructor from newsitems class-based component");
     }
     async updateNews() {
+        this.props.setProgress(10);
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=868c2e50168b4e3da891de01e3657fba&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
+        this.props.setProgress(70);
         console.log(parsedData);
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
